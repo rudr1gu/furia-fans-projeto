918ecac3671aac3e16b429532016d2bcad8ff0e4
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-25 15:20:34 -0300

    :sparkles: feat: implement UsuarioService for user management API interactions

---
+++ src/services/UsuarioService.ts
@@ -0,0 +1,36 @@
+import Usuario from "../models/Usuario";
+import baseApi from "./baseApi";
+
+class UsuarioService {
+
+    createUsuario = async(usuario: Usuario, setDados: Function) => {
+        const response = await baseApi.post<Usuario>('/usuarios/cadastrar', usuario);
+        setDados(response.data);
+    }
+
+    getAllUsuarios = async(setDados: Function) => {
+        const response = await baseApi.get<Usuario[]>('/usuarios/all');
+        setDados(response.data);
+    }
+
+    loginUsuario = async(usuario: Usuario, setDados: Function) => {
+        const response = await baseApi.post<Usuario>('/usuarios/logar', usuario);
+        setDados(response.data);
+    }
+
+    getByIdUsuario = async(id: number, setDados: Function) => {
+        const response = await baseApi.get<Usuario>(`/usuarios/${id}`);
+        setDados(response.data);
+    }
+
+    updateUsuario = async(usuario: Usuario, setDados: Function) => {
+        const response = await baseApi.put<Usuario>(`/usuarios/atualizar`, usuario);
+        setDados(response.data);
+    }
+    
+    deleteUsuario = async(id: number, setDados: Function) => {
+        const response = await baseApi.delete<Usuario>(`/usuarios/deletar/${id}`);
+        setDados(response.data);
+    }
+}
+export default UsuarioService
