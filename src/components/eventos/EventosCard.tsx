6dd3513de5cbb2fa2c00f3dce3e2d8a7db99ace0
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-27 13:16:16 -0300

    :sparkles: feat: update EventosCard styling; enhance text colors and replace anchor with button for better accessibility

--- src/components/eventos/EventosCard.tsx
+++ src/components/eventos/EventosCard.tsx
@@ -7,8 +7,10 @@ interface EventosCardProps {
 }
 
 const EventosCard: React.FC<EventosCardProps> = ({ evento }) => {
+
+
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-[1.02]">
+    <div className="bg-white dark:bg-zinc-900 rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-[1.02]">
       <div className="h-48 overflow-hidden">
         <img 
           src={evento.imagemUrl} 
@@ -17,27 +19,27 @@ const EventosCard: React.FC<EventosCardProps> = ({ evento }) => {
         />
       </div>
       <div className="p-4">
-        <h3 className="text-xl font-bold text-gray-900 dark:text-white mb-2">{evento.titulo}</h3>
-        <p className="text-gray-600 dark:text-gray-300 mb-4 line-clamp-2">{evento.descricao}</p>
+        <h3 className="text-xl font-bold text-zinc-900 dark:text-white mb-2">{evento.titulo}</h3>
+        <p className="text-zinc-600 dark:text-zinc-300 mb-4 line-clamp-2">{evento.descricao}</p>
         
         <div className="space-y-2">
-          <div className="flex items-center text-gray-700 dark:text-gray-300">
+          <div className="flex items-center text-zinc-900 dark:text-zinc-300">
             <Calendar size={18} className="mr-2" />
             <span>{evento.dataInicio}</span>
           </div>
-          <div className="flex items-center text-gray-700 dark:text-gray-300">
+          <div className="flex items-center text-zinc-900 dark:text-zinc-300">
             <Clock size={18} className="mr-2" />
             <span>{evento.horaInicio}</span>
           </div>
-          <div className="flex items-center text-gray-700 dark:text-gray-300">
+          <div className="flex items-center text-zinc-900 dark:text-zinc-300">
             <MapPin size={18} className="mr-2" />
             <span>{evento.localizacao}</span>
           </div>
         </div>
         
-        <a className="mt-4 w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition-colors duration-300" href={evento.link} target="_blank" rel="noopener noreferrer">
+        <button className="mt-4 w-full bg-black text-white py-2 hover:bg-zinc-950 transition-colors duration-300" rel="noopener noreferrer">
           Saber mais
-        </a>
+        </button>
       </div>
     </div>
   );
