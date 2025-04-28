418ee607ebc6220d9771e1724b3a3a26c8801b58
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-28 14:20:46 -0300

    :sparkles: feat: update Login component; replace logo image and add registration link for improved user navigation

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent, useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import UsuarioLogin from "../../models/UsuarioLogin";
-import logo from "../../assets/logo-furia.svg";
+import logoFuria from "../../assets/furia.png";
 
 const Login = () => {
 
@@ -33,7 +33,7 @@ const Login = () => {
         <section className="min-h-screen flex flex-col justify-center items-center bg-zinc-200 dark:bg-zinc-900 transition-colors">
             <form onSubmit={login} className="w-full max-w-md p-8 rounded-lg shadow-lg bg-white dark:bg-zinc-950 transition-colors">
                 <div className="flex justify-center mb-1">
-                    <img src={logo} alt="Logo" className="w-20 h-20" />
+                    <img src={logoFuria} alt="Logo" className="w-20 h-20" />
                 </div>
                 <h2 className="text-3xl font-bold mb-6 text-center text-black dark:text-white">Login</h2>
 
@@ -72,6 +72,13 @@ const Login = () => {
                 >
                     {isLoading ? "Logando..." : "Entrar"}
                 </button>
+                <p className="mt-4 text-sm text-center text-black dark:text-white">
+                    Não tem uma conta?{" "}
+                    <Link to="/cadastro" className="text-blue-500 hover:underline">
+                        Cadastre-se
+                    </Link>
+                </p>
+
             </form>
         </section>
 
