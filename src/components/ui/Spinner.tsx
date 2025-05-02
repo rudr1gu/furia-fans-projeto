f3d3dc87f152fe3c38cd1c270be5e240403b49fd
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-05-02 11:36:58 -0300

    :sparkles: feat: update Spinner component styles for improved visual consistency

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
     return (
-        <span className="relative w-6 h-6 rounded-full border-4 border-black border-t-transparent border-r-transparent animate-spin">
-            <span className="absolute inset-0 m-auto w-6 h-6 rounded-full border-4 border-white border-t-transparent animate-spin-reverse" />
+        <span className="relative w-6 h-6 rounded-full border-4 border-zinc-400 border-t-transparent border-r-transparent animate-spin">
+            <span className="absolute inset-0 m-auto w-3 h-3 rounded-full border-4 border-white border-t-transparent animate-spin-reverse" />
         </span>
     );
 }
